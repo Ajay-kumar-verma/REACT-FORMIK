65e615b8cbe5fc1d4093370cb284c316775484a2
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-09-08 22:32:54 +0530

    formik

---
+++ formik_form/src/App.js
@@ -0,0 +1,10 @@
+import X from './Form'; 
+function App() {
+ return (
+<>
+<div>Ajay kumar verma</div>
+ <X />
+</>  );
+}
+
+export default App;
---
+++ formik_form/src/Form.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useFormik } from 'formik';
+
+ const Form = () => {
+    const initialValues= {
+    firstName: '',
+    lastName: 'kumar',
+    email: 'aja',
+  }
+
+  const  onSubmit= values => {
+    alert(JSON.stringify(values, null, 2));
+  }
+
+const validate=(values)=>{
+// it keep tracking the data and used for setting the errors 
+// console.log("ajay klumar verma")
+const errors= {};
+if(values.firstName==="") 
+  errors["firstName"]="Fill kr de bhai ";
+
+ return errors;
+  }
+  const formik = useFormik({initialValues,onSubmit,validate});
+// formik return all object we need to manipluate the form 
+//   in formik.Values all values contain  
+// formik keep trtacking everything // 
+// it validate and make errors objec also 
+console.log("form values ",formik.values);
+console.log("Touched  values ",formik.touched);
+useEffect(()=>{
+    console.log("Formik returned object ",formik)
+},[formik])
+
+// whenever make anychange in the form this above part gets executed 
+
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+        onBlur={formik.handleBlur}
+     
+     />
+      {formik.touched.firstName && formik.errors.firstName && <div>{formik.errors.firstName}</div>}
+  <br/>
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+        onBlur={formik.handleBlur}
+     
+     />
+
+<br/>
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+<br/>
+  
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
